95f657030b01c6fa5b175d32af722f37226e676d
frechanique-minsait <39118713+frechanique-minsait@users.noreply.github.com>
2018-05-09 13:46:55 +0200

    Update rest.js

--- rest.js
+++ rest.js
@@ -3,6 +3,10 @@ app.get('/process1/:data', function(req, res){
 	var data = req.params.data
 	res.send( JSON.stringify( {'response':'ok!!'} ) );
 });
+app.get('/data', function(req, res){
+	var data = req.params.data
+	res.send( JSON.stringify( {'response':'oka'} ) );
+});
 //PUT
 app.put('/process1/:data', function(req, res){
 	var data = req.params.data
